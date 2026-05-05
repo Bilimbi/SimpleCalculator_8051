b92a4fc1bcb9acc4afa0ef6204444e96fa61a2a8
0dium <238311524+0diuuum@users.noreply.github.com>
2026-05-05 11:06:25 +0200

    Added something

--- calculator.asm
+++ calculator.asm
@@ -1,7 +1,27 @@
     LJMP START
-    ORG 100H ; Jump to the start of the program
+    ORG 100H ; Jump over debuger to the start of the program
 START:
-    ; Main program (work in progress)
+    LCALL LCD_CLR
+	MOV SP,#60H ; Set stack adress to the 60H
+
+    ; Get the first number
+    LCALL WAIT_KEY
+    PUSH ACC ; Store the first number on the stack
+    LCALL WRITE_HEX
+
+    ; Get the operation
+    LCALL WAIT_KEY
+
+; \/ Operations \/ ---------------------------
+
+ADD_FUNC: ; Addition Function 
+
+SUB_FUNC: ; Substraction Function
+
+MUL_FUNC: ; Multiplication Function
+
+DIV_FUNC: ; Division Function
+
 STOP:
     SJMP $ ; Stopping the program
     NOP ; No operation for the program counter
