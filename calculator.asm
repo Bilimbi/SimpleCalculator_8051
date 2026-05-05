9b9e1d4fdb2f93985eeb7b882b3ced3cc3db6939
0dium <238311524+0diuuum@users.noreply.github.com>
2026-05-05 09:08:39 +0200

    Initial Commit

---
+++ calculator.asm
@@ -0,0 +1,7 @@
+    LJMP START
+    ORG 100H ; Jump to the start of the program
+START:
+    ; Main program (work in progress)
+STOP:
+    SJMP $ ; Stopping the program
+    NOP ; No operation for the program counter
